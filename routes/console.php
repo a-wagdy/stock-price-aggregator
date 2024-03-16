fd87aac006bc6e73fc9e1cc05939abe1c3ca1bc2
Ahmed Wagdy <ahmed@pelcro.com>
2024-03-16 16:39:48 +0200

    Scheduled the command to run every minute

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Jobs\PopulateRealTimePrices;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::job(new PopulateRealTimePrices())->everyMinute()->withoutOverlapping();
