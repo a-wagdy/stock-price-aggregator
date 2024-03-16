2891ecda8f179f43ab0546e75598b75002054b68
Ahmed Wagdy <ahmed@pelcro.com>
2024-03-16 21:27:50 +0200

    Used DI for better testability

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,8 @@
 
 declare(strict_types=1);
 
+use App\API\AlphaVantageApiService;
 use App\Jobs\PopulateRealTimePrices;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(new PopulateRealTimePrices())->everyMinute()->withoutOverlapping();
+Schedule::job(new PopulateRealTimePrices(new AlphaVantageApiService()))->everyMinute()->withoutOverlapping();
