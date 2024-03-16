59b5c21c27735d840e3b7821f77a5fc3a3965023
Ahmed Wagdy <ahmed@pelcro.com>
2024-03-16 16:32:19 +0200

    Missing file from previous commit

--- app/Stock/API/AlphaVantageAPI.php
+++ app/Stock/API/AlphaVantageAPI.php
@@ -9,6 +9,30 @@ class AlphaVantageAPI
     public const API_ENDPOINT = 'https://alphavantage.co/query';
 
     /**
+     * Get the latest price for symbol
+     *
+     * @param string $symbol
+     * @return float
+     */
+    public function getCurrentPriceForQuote(string $symbol): float
+    {
+        $endpoint = $this->getQuoteEndpoint($symbol);
+        $payload = $this->getContents($endpoint);
+
+        if (!isset($payload['Global Quote']) || !isset($payload['Global Quote']['05. price'])) {
+            throw new Exception('Encountered unexpected payload');
+        }
+
+        $currentPrice =  $payload['Global Quote']['05. price'];
+
+        if (!\is_numeric($currentPrice)) {
+            throw new Exception("Error getting current price for quote {$symbol}");
+        }
+
+        return (float) $currentPrice;
+    }
+
+    /**
      * Get the quote endpoint URL
      *
      * @param string $symbol
